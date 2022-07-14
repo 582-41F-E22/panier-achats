a08898df60dab6ef2db46da6659c39085b868a51
Camille Semaan <cdem@xvitae.com>
2022-07-14 15:00:52 -0400

    Exemple d'intégration Firestore

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -1,8 +1,10 @@
 import './ListeProduits.scss';
 import Produit from './Produit';
-import tabProduits from './data/produits.json';
+// import tabProduits from './data/produits.json';
 import { Link } from "react-router-dom";
 import {bdFirestore} from './firebase/init';
+import { collection, getDocs } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
 
 export default function ListeProduits({panier, setPanier}) {
   // console.log('Tableau des produits : ', tabProduits);
@@ -11,11 +13,19 @@ export default function ListeProduits({panier, setPanier}) {
   //   tabJsxProduits[i] = <Produit id={tabProduits[i].id} nom={tabProduits[i].nom} prix={tabProduits[i].prix} />;
   // }
 
+  const [produits, setProduits] = useState([]);
+
+  useEffect(()=>
+    () => getDocs(collection(bdFirestore, 'produits')).then(
+      qs => setProduits(qs.docs.map(doc => ({id: doc.id, ...doc.data()})))
+    )
+  , []);
+
   return (
     <main className="ListeProduits">
       <h2>Produits disponibles</h2>
       <section>
-        {tabProduits.map(
+        {produits.map(
           prd => 
             <Link key={prd.id} to={`/nos-produits/${prd.id}`}>
               <Produit panier={panier} setPanier={setPanier} id={prd.id} nom={prd.nom} prix={prd.prix}/>
